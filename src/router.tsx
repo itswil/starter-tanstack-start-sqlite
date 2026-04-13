3f5444665a83b77c3d2337b96a0972b467ca0f6a
itswil <ww8668@gmail.com>
2026-04-13 21:18:48 +0800

    chore: add notFoundComponent

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,11 @@ export function getRouter() {
   const router = createRouter({
     routeTree,
     scrollRestoration: true,
+    defaultNotFoundComponent: () => (
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+      </div>
+    ),
   });
 
   return router;
